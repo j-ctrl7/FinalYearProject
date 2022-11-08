673e90012efde2b31b219b5e901fb41403b91cdc
Alfred AKinkoye <63430653+Alfred-Akinkoye@users.noreply.github.com>
2022-11-08 16:09:00 -0500

    Update StartDB.py

--- Database/StartDB.py
+++ Database/StartDB.py
@@ -18,6 +18,7 @@ def create_connection(db_name):
         conn = mysql.connector.connect(user='root', password='CapStone_2023',
                                       host='127.0.0.1',
                                       database=db_name)
+        return conn
     except mysql.connector.Error as err:
         if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
             print("Something is wrong with your user name or password")
@@ -25,23 +26,28 @@ def create_connection(db_name):
             print("Database does not exist")
         else:
             print(err)
-    return conn
 
 #Creating user Table
-def create_userdata_table(conn):
-    Table_string = """ CREATE TABLE USERS (
+def create_userdata_table(conn, username):
+    #Creates a Table for the user as long as the username doesn't already exist
+    Table_string = """ CREATE TABLE IF NOT EXISTS STYLE_POINTS (
                                         username char(35) NOT NULL,
-                                        password char(35) NOT NULL
+                                        password char(35) NOT NULL,
+                                        email char(35) NOT NULL,
                                     ); """
     try:
         cursor = conn.cursor()
         cursor.execute(Table_string)
-    except Error as e:
-        print(e)
+    except mysql.connector.Error as err:
+        print(err)
 
 def main():
     conn = create_connection("user_db")
-    create_userdata_table(conn)
+    create_userdata_table(conn,"STYLE_POINTS")
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM USERS")
+    result = cursor.fetchall()
+    print(result)
 
 if __name__ == "__main__":
     main()
